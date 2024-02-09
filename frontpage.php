33fb541d283c30865c4cd8b40890a37e33edb69c
Myzwer <josh@onefortyfivedesign.com>
2024-02-09 02:32:09 -0500

    :tada: Initial Commit - Based off of Pre-Launch Theme so the base template is already there.

---
+++ frontpage.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template Name: Custom - Front Page
+ *
+ * The Frontpage of the PreLaunch Theme
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Lets_Go_BBY
+ * @author Josh Forrester <josh@onefortyfivedesign.com>
+ * @version 1.0.0
+ */
+
+get_header(); ?>
+
+    <video class="header-video" src="https://foothillscollective.com/wp-content/uploads/2021/04/Res-Power-Background.mp4" autoplay loop playsinline muted></video>
+
+    <div class="viewport-header">
+        <div class="head-container">
+            <div class="center add-padding">
+                <h1 class="text-white text-5xl pb-5">Header Title</h1>
+            </div>
+            <hr class="text-white pb-5">
+            <h2 class="text-white text-3xl ">Title</h2>
+            <h3 class="text-white text-2xl">Subtitle</h3>
+        </div>
+    </div>
+
+    <div class="bg-white md:p-10">
+        <div class=" lg:max-w-5xl lg:mx-auto">
+            <div class="grid grid-cols-12 gap-4 md:gap-10">
+                <div class="col-span-12 md:col-span-4">
+                    <?php get_template_part( 'components/cards/_sample-card' ); ?>
+                </div>
+
+                <div class="col-span-12 md:col-span-4">
+                    <div class="prose">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto error et laudantium molestiae odit tempora temporibus? Aliquid aspernatur aut autem dolore, enim minima molestias sapiente. Consectetur culpa nihil obcaecati?</div>
+                </div>
+
+                <div class="col-span-12 md:col-span-4 prose">
+                    <h2>Header Line</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores earum magni nihil praesentium quam sequi sunt vitae. Excepturi iure molestias perferendis quidem, recusandae tempora. A hic nihil pariatur suscipit ut.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+<?php
+get_footer();
---
+++ functions.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * The primary functions file for the theme. It includes various modular files
+ * responsible for handling script and style enqueue, Advanced Custom Fields (ACF) configurations,
+ * quick utility functions, WordPress menu setups, post-related functions and pagination,
+ * custom post type registrations, and shortcodes.
+ *
+ *
+ * Usage: Wordpress defaults to use this file.
+ *
+ * @package WordPress
+ * @subpackage Lets_Go_BBY
+ * @author Josh Forrester <josh@onefortyfivedesign.com>
+ * @version 1.0.0
+ */
+
+
+// Include Scripts and Styles
+require_once get_template_directory() . '/includes/enqueue.php';
+
+// Include ACF Functions
+require_once get_template_directory() . '/includes/acf.php';
+
+// Include Quick Functions
+require_once get_template_directory() . '/includes/quick_functions.php';
+
+// Include WP Menus (Header and Footer)
+require_once get_template_directory() . '/includes/menus.php';
+
+// Include Posts (Post Functions + Pagination)
+require_once get_template_directory() . '/includes/wpposts.php';
+
+// Include Custom Post Types
+require_once get_template_directory() . '/includes/custom_post_types.php';
+
+// Include Shortcodes
+require_once get_template_directory() . '/includes/shortcodes.php';
