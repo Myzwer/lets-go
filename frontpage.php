cc803a491fcbfbf1e36db3540145e1e1d5a7ea5c
Myzwer <josh@onefortyfivedesign.com>
2024-02-15 09:41:35 -0500

    :rocket: Added the orange wave and the beginning of the next section.

--- frontpage.php
+++ frontpage.php
@@ -14,13 +14,16 @@
 
 get_header(); ?>
 
+    <!-- Start Particles-->
+    <div id="particles-js"></div>
+    <!-- End particles -->
 
-    <div class="bg-white-gradient relative py-10 md:py-20">
+    <div class="bg-white-gradient relative pt-10 md:pt-20">
         <div class="absolute block -top-20 md:top-0 -left-2">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/triangle-1.png" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/src/img/triangle-1.png" alt="">
         </div>
-        
-        <div class="grid grid-cols-12 lg:max-w-7xl mx-auto pt-5 gap-4 md:gap-14 relative">
+
+        <div class="grid grid-cols-12 lg:max-w-7xl mx-auto pt-5 gap-4 md:gap-14 relative z-10">
 
             <!-- Lets Go Logo-->
             <div class="col-span-12 md:col-span-8 lg:col-span-5 mx-5">
@@ -31,11 +34,11 @@ get_header(); ?>
             <div class="col-span-12 lg:col-span-7 mx-5">
 
                 <!-- Big Ass Text-->
-                <h1 class = "text-3xl md:text-5xl 2xl:text-7xl uppercase druk"><?php the_field('main_text'); ?></h1>
+                <h1 class="text-3xl md:text-5xl 2xl:text-7xl uppercase druk"><?php the_field('main_text'); ?></h1>
 
                 <!-- Button-->
-                <?php if( have_rows('primary_cta') ): ?>
-                    <?php while( have_rows('primary_cta') ): the_row(); ?>
+                <?php if (have_rows('primary_cta')): ?>
+                    <?php while (have_rows('primary_cta')): the_row(); ?>
                         <a href="<?php the_sub_field("button_link"); ?>">
                             <button class="fab-main mt-3">
                                 <i class="fa-solid fa-circle-arrow-right"></i> <?php the_sub_field("button_text"); ?>
@@ -46,8 +49,20 @@ get_header(); ?>
             </div>
 
         </div>
+
+        <!--        <img class="w-full object-cover" src="-->
+        <?php //echo get_template_directory_uri(); ?><!--/assets/src/img/orange-wave.png" alt="">-->
+
+        <div class="bg-no-repeat bg-scroll bg-cover relative z-10"
+             style="background:
+                     url('<?php echo get_template_directory_uri(); ?>/assets/src/img/orange-wave.png') center center;
+                     height: 20vh; background-repeat: no-repeat; background-size: cover;">
+        </div>
     </div>
 
+    <div class="bg-orange relative -mt-1">
+        <div class="py-64">content</div>
+    </div>
 
 
 <?php get_footer();
