a850837113c4b9d5f84f4aa6b96280cbb09aee61
Myzwer <josh@onefortyfivedesign.com>
2024-02-24 01:53:13 -0500

    :art: Redesigned the checkout page to loosely resemble a shopify checkout page since users are familiar with that UI.

--- checkout.php
+++ checkout.php
@@ -16,85 +16,54 @@ get_header(); ?>
 // Get the event ID
 $event_id = get_event_id();
 ?>
-<?php get_template_part( 'components/headers/_simple' );  ?>
-
 
 
     <div class="bg-white text-black relative">
-        <div class="grid grid-cols-12 gap-4 pb-10 px-5">
-            <div class="col-span-12 md:col-span-10 lg:col-start-3 lg:col-span-8">
-                <div class="mt-5">
-                    <div class="table-auto">
-                        <?php the_content(); ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+        <div class="grid grid-cols-12 gap-4 md:gap-20 pb-10 px-5 lg:max-w-7xl mx-auto">
 
-    <!-- START SECTION 3 -->
-    <div class="bg-blue relative -mt-1 z-10">
-
-        <div class="grid grid-cols-12 lg:max-w-7xl gap-4 relative z-10 mx-6 xl:mx-auto">
-
-            <!-- Title -->
             <div class="col-span-12 md:col-span-8">
-                <div class="drop-shadow">
-                    <h1 class="text-3xl md:text-5xl uppercase druk text-white">Event Details</h1>
+                <div class="signup">
+                    <h2 class="text-black text-xl md:text-2xl font-bold uppercase pt-10 -mb-20"><?php echo get_the_title(); ?></h2>
+                    <?php the_content(); ?>
                 </div>
             </div>
 
-            <!-- Content -->
-            <div class="col-span-12 mt-5">
-                <div class="grid grid-cols-12">
-                    <div class="col-span-2 mt-5 text-4xl">
-                        <i class="fa-solid fa-location-dot"></i>
-                    </div>
+            <div class="col-span-12 md:col-span-4">
+                <div class="details bg-blue mt-5 p-5">
+                    <?php the_content(); ?>
+                    <div class="grid grid-cols-12 lg:max-w-7xl gap-4 relative z-10 mx-6 xl:mx-auto">
+                        <!-- Title -->
+                        <div class="col-span-12 md:col-span-12">
+                            <h3 class="text-xl uppercase druk text-white">Event Details</h3>
+                        </div>
 
-                    <div class="col-span-8 mt-5">
-                        <p class="pt-2 text-lg text-white font-bold text-left"><?php the_field('address', $event_id); ?></p>
+
+                        <!-- Icons + Content -->
+                        <div class="col-span-12 mt-3">
+                            <div class="flex items-center text-white">
+                                <i class="fa-solid fa-location-dot text-4xl flex-none w-12"></i>
+                                <p class="ml-5 text-lg text-white font-bold"><?php the_field('address', $event_id); ?></p>
+                            </div>
+                        </div>
+
+                        <div class="col-span-12 mt-3">
+                            <div class="flex items-center text-white">
+                                <i class="fa-solid fa-family text-4xl flex-none w-12"></i>
+                                <p class="ml-5 text-lg text-white font-bold"><?php the_field('children_restrictions', $event_id); ?></p>
+                            </div>
+                        </div>
+
+                        <div class="col-span-12 mt-3">
+                            <div class="flex items-center text-white">
+                                <i class="fa-solid fa-clouds-sun text-4xl flex-none w-12"></i>
+                                <p class="ml-5 text-lg text-white font-bold"><?php the_field('weather', $event_id); ?></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="col-span-12 mt-5">
-                <div class="pb-5">
-                    <div class="drop-shadow">
-                        <h2 class="text-2xl druk text-white">Family Restrictions</h2>
-                    </div>
-                    <p class="pt-2 text-lg text-white font-bold"><?php the_field('children_restrictions', $event_id); ?></p>
-                </div>
-            </div>
-
-            <div class="col-span-12 mt-5">
-                <div class="pb-5">
-                    <div class="drop-shadow">
-                        <h2 class="text-2xl druk text-white">Weather Details</h2>
-                    </div>
-                    <p class="pt-2 text-lg text-white font-bold"><?php the_field('weather', $event_id); ?></p>
-                </div>
-            </div>
-
-            <!-- Button-->
-            <div class="col-span-12 mb-5">
-                <a href="<?php the_field('serve_link', 'options'); ?>" target="_blank">
-                    <button class="ghost-white">
-                        <?php the_field("button_text"); ?>
-                    </button>
-                </a>
-            </div>
-        </div>
-
-        <!-- The Wave -->
-        <div class="bg-no-repeat bg-scroll bg-cover relative z-5"
-             style="background:
-                     url('<?php echo get_template_directory_uri(); ?>/assets/src/img/black-wave.png') center center no-repeat;
-                     height: 20vh; background-size: cover;">
         </div>
     </div>
-    <!-- END SECTION 3 -->
-
-
 
 
 <?php
