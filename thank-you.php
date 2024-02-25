665e7396ea8221487330dd1231112b409bf1f4ea
Myzwer <josh@onefortyfivedesign.com>
2024-02-25 08:10:36 -0500

    :art: Styled the thank you page to look like the mockup. Moved my css out of the main event espresso style file to individual files.

--- thank-you.php
+++ thank-you.php
@@ -13,75 +13,26 @@
 get_header(); ?>
 
 
-<?php get_template_part( 'components/headers/_simple' );  ?>
-
-
-
     <div class="bg-white text-black relative">
-        <div class="grid grid-cols-12 gap-4 pb-10 px-5">
+        <div class="grid grid-cols-12 gap-4 py-10 px-5">
+            <div class="col-span-12 text-center">
+                <h1 class="text-black text-3xl md:text-5xl font-bold uppercase">Registration Confirmed</h1>
+                <h3 class="text-black text-2xl font-bold uppercase pt-5">Let's Go Serve!</h3>
+
+            </div>
+
             <div class="col-span-12 md:col-span-10 lg:col-start-3 lg:col-span-8">
                 <div class="mt-5">
-                    <div class="table-auto">
+                    <div class="thank-you">
                         <?php the_content(); ?>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
 
-    <!-- START SECTION 3 -->
-    <div class="bg-blue relative -mt-1 z-10">
 
-        <div class="grid grid-cols-12 lg:max-w-7xl gap-4 relative z-10 mx-6 xl:mx-auto">
 
-            <!-- Title -->
-            <div class="col-span-12 md:col-span-8">
-                <div class="drop-shadow">
-                    <h1 class="text-3xl md:text-5xl uppercase druk text-white">Event Details</h1>
-                </div>
-            </div>
-
-            <!-- Content -->
-            <div class="col-span-12 mt-5">
-                <div class="grid grid-cols-12">
-                    <div class="col-span-2 mt-5 text-4xl">
-                        <i class="fa-solid fa-location-dot"></i>
-                    </div>
-
-                    <div class="col-span-8 mt-5">
-                        <p class="pt-2 text-lg text-white font-bold text-left"><?php the_field('address'); ?></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-span-12 mt-5">
-                <div class="pb-5">
-                    <div class="drop-shadow">
-                        <h2 class="text-2xl druk text-white">Family Restrictions</h2>
-                    </div>
-                    <p class="pt-2 text-lg text-white font-bold"><?php the_field('children_restrictions', $post->ID); ?></p>
-                </div>
-            </div>
-
-            <div class="col-span-12 mt-5">
-                <div class="pb-5">
-                    <div class="drop-shadow">
-                        <h2 class="text-2xl druk text-white">Weather Details</h2>
-                    </div>
-                    <p class="pt-2 text-lg text-white font-bold"><?php the_field('weather', $post->ID); ?></p>
-                </div>
-            </div>
-
-            <!-- Button-->
-            <div class="col-span-12 mb-5">
-                <a href="<?php the_field('serve_link', 'options'); ?>" target="_blank">
-                    <button class="ghost-white">
-                        <?php the_field("button_text"); ?>
-                    </button>
-                </a>
-            </div>
-        </div>
 
         <!-- The Wave -->
         <div class="bg-no-repeat bg-scroll bg-cover relative z-5"
@@ -90,6 +41,7 @@ get_header(); ?>
                  height: 20vh; background-size: cover;">
         </div>
     </div>
+
     <!-- END SECTION 3 -->
 
 
