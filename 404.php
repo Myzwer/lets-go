17369160023473476a92462d62df8ae05f85fd40
Myzwer <josh@onefortyfivedesign.com>
2024-02-26 10:49:51 -0500

    :rocket: Modified to 404 page to be accurate and have a correct homepage link

--- 404.php
+++ 404.php
@@ -15,27 +15,24 @@
 get_header(); ?>
 
     <div class="grid grid-cols-12">
-        <div class="col-span-12 md:col-span-6 text-center">
-            <div class="md:text-left md:mx-10">
-                <div class="w-20 mt-11">
-                    <img src="https://images.squarespace-cdn.com/content/v1/575a6067b654f9b902f452f4/1552683653140-0UUVQSSUEWVC73AWAEQG/300Logo.png"
-                         alt="Company Logo">
-                </div>
-
-                <p class="md:mt-10">404 Error</p>
-                <h1 class="uppercase text-2xl md:text-3xl md:mb-10">Page Not Found</h1>
-                <p>The rocket took off without this page ¯\_(ツ)_/¯</p>
-                <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                    Back to Homepage
+        <div class="col-span-12 md:col-span-6 text-center relative">
+            <div class="content-middle-medium">
+                <p class="md:mt-10 text-lg">404 Error</p>
+                <h1 class="uppercase text-2xl md:text-3xl ">Page Not Found - Broken Link</h1>
+                <p>Don't worry, there's still plenty of opportunities to serve!</p>
+            <a href="/">
+                <button class="fab-main mt-3">
+                    <i class="fa-solid fa-circle-arrow-right"></i> See all Opportunities
                 </button>
-            </div>
+            </a>
         </div>
+    </div>
 
         <div class="col-span-12 md:col-span-6">
             <div class="bg-no-repeat bg-scroll bg-cover relative" style="background: linear-gradient(
 				  rgba(0, 0, 0, 0.45),
 				  rgba(0, 0, 0, 0.45)
-				), url('https://images.unsplash.com/photo-1501612780327-45045538702b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80') center center;
+				), url('https://images.unsplash.com/photo-1604079628040-94301bb21b91?q=80&w=2731&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') center center;
 				 height: 80vh;">
             </div>
         </div>
